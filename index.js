aa255a50ffddd39a18c8e7fcbf438c282e97f0c4
van vinh <vinhpv@kyc.net.vn>
2020-12-10 16:52:46 +0700

    commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var http = require('http');
 var app = express()
 var router = express.Router();
 var tes = require('./route/test')
@@ -7,6 +8,7 @@ app.use('/', tes)
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 app.listen('7000',function(){
     console.log('running port 7000')
 })
